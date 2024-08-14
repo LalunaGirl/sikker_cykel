84610d36b8ca2978a1566e8bbd1f542795b96e17
Nina <nina.thomsen@hotmail.com>
2024-08-14 17:16:09 +0200

    Added firebase api keys to secret .env file

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -8,4 +8,13 @@ part 'env.g.dart';
 abstract class Env {
   @EnviedField(varName: 'TESTKEY', obfuscate: true)
   static final String testKey = _Env.testKey;
+
+  @EnviedField(varName: 'WEB_API_KEY', obfuscate: true)
+  static final String webApiKey = _Env.webApiKey;
+
+  @EnviedField(varName: 'ANDROID_API_KEY', obfuscate: true)
+  static final String androidApiKey = _Env.androidApiKey;
+
+  @EnviedField(varName: 'IOS_API_KEY', obfuscate: true)
+  static final String iosApiKey = _Env.iosApiKey;
 }
